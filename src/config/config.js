1299f93dfe1680a5094de0955e6cd9e0d86b03f2
kaiqigong <cagegong@gmail.com>
2016-06-17 12:50:43 +0800

    feat(weixin): wechat redirect to test

--- src/config/config.js
+++ src/config/config.js
@@ -74,7 +74,7 @@ const config = {
     },
     cookieExpire: 7 * 24 * 3600 * 1000, // 7天
     cookieDomain: '.learnwithwind.com',
-    host: 'www.holdqq.com/test/',
+    host: 'www.holdqq.com/test',
     weixin: {
       appid: 'wx453346560ed6a84e',
       secret: '2e1364c00766952f641b197b67dd3b9c',
