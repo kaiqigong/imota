0175338daa35aaf25689b59e2827b955fda48425
kaiqigong <cagegong@gmail.com>
2016-06-21 18:50:16 +0800

    fix(learningHistory): calc script

--- src/app/crons/calcLearningTime.js
+++ src/app/crons/calcLearningTime.js
@@ -71,7 +71,7 @@ const calc = async () => {
       });
       learningHistory.learningTime = learningTime / 1000 / 60;
       console.log(learningHistory);
-      const lastLearningHistory = await LearningHistory.findOne({accountId}).sort({created: -1}).exec();
+      const lastLearningHistory = await LearningHistory.findOne({accountId, created: {$lt: date}}).sort({created: -1}).exec();
       console.log(lastLearningHistory);
       if (lastLearningHistory && lastLearningHistory.totalLearningTime) {
         learningHistory.totalLearningTime = lastLearningHistory.totalLearningTime + learningHistory.learningTime;
